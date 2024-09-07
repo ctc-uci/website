e4e7dd0fa44607bdab5ded98c4da70b93e5d7d5d
Kevin Wu <kevinwu098@gmail.com>
2024-09-07 10:18:49 -0700

    feat: scaffold hero component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,58 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+import { ApplyButton } from "../header/ApplyButton";
+
 export const Hero = () => {
   return (
-    <div>
-      <div>Hero</div>
-    </div>
+    <Flex
+      sx={{
+        paddingX: { base: "20px", lg: "100px" },
+        paddingY: 150,
+        justifyContent: "space-between",
+        height: 750,
+      }}
+    >
+      <Stack
+        spacing={5}
+        sx={{ maxWidth: "50%" }}
+      >
+        <Heading
+          sx={{ color: "ctc.purple", fontSize: "8xl", lineHeight: "normal" }}
+        >
+          Commit the Change
+        </Heading>
+        <Text sx={{ fontSize: "4xl" }}>Creating tech for social good.</Text>
+
+        <HStack>
+          <ApplyButton />
+          <Button
+            variant={"solid"}
+            sx={{
+              backgroundColor: "#DADADA",
+              _hover: {
+                bg: "#DADADA",
+                opacity: 0.9,
+              },
+              _active: {
+                bg: "#DADADA",
+                opacity: 0.8,
+              },
+            }}
+          >
+            Learn More
+          </Button>
+        </HStack>
+      </Stack>
+
+      <Box sx={{ width: 700, backgroundColor: "ctc.purple", height: 600 }} />
+    </Flex>
   );
 };
