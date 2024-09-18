f3e9302c05257991a829f0d3a8aa90eafa5c4d47
Kevin Wu <kevinwu098@gmail.com>
2024-09-17 22:50:22 -0700

    feat: questions

---
+++ src/components/apply/questions/Questions.tsx
@@ -0,0 +1,52 @@
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+import { headingStyle } from "../../../styles/shared";
+import { QUESTIONS } from "./questions-data";
+
+export function Questions() {
+  return (
+    <Stack spacing={8}>
+      <Stack spacing={5}>
+        <Heading sx={headingStyle}>Questions?</Heading>
+        <Text sx={{ fontSize: "lg", color: "ctc.text" }}>
+          Couldn’t find your answer? Email us at ctc@uci.edu, or reach out to us
+          on any of our socials.
+        </Text>
+      </Stack>
+
+      <Accordion
+        defaultIndex={[0]}
+        allowMultiple
+        allowToggle
+      >
+        {QUESTIONS.map((question) => (
+          <AccordionItem key={question.question}>
+            <AccordionButton sx={{ display: "flex", gap: 5, paddingY: 3 }}>
+              <AccordionIcon
+                sx={{ color: "ctc.purple", width: 6, height: 6 }}
+              />
+
+              <Text sx={{ fontSize: "lg", color: "ctc.text" }}>
+                {question.question}
+              </Text>
+            </AccordionButton>
+            <AccordionPanel sx={{ whiteSpace: "pre-wrap", paddingBottom: 6 }}>
+              <Text sx={{ fontSize: "md", lineHeight: 4 }}>
+                {question.answer}
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </Stack>
+  );
+}
---
+++ src/components/apply/questions/questions-data.ts
@@ -0,0 +1,37 @@
+type Question = {
+  question: string;
+  answer: string;
+};
+
+export const QUESTIONS: Question[] = [
+  {
+    question: "What can I expect as a member of CTC?",
+    answer: `We aim to provide members of CTC with hands-on web development experience, in addition to bridging the knowledge gap between college courses and industry practice! \n\nDesigners will learn hard skills such as research, wireframing, prototyping in Figma, and usability testing. Developers will get hands on experience with full-stack technologies and version control. \n\nCTC also provides a community of passionate, like-minded people that support each other through the learning process & life in general. As a member, you can expect team and club-wide socials, as well as a welcoming community!`,
+  },
+  {
+    question: "Who can join Commit the Change?",
+    answer: `We aim to provide members of CTC with hands-on web development experience, in addition to bridging the knowledge gap between college courses and industry practice! \n\nDesigners will learn hard skills such as research, wireframing, prototyping in Figma, and usability testing. Developers will get hands on experience with full-stack technologies and version control. \n\nCTC also provides a community of passionate, like-minded people that support each other through the learning process & life in general. As a member, you can expect team and club-wide socials, as well as a welcoming community!`,
+  },
+  {
+    question:
+      "If I am accepted to join Commit the Change, how will I be matched to a project?",
+    answer: `We aim to provide members of CTC with hands-on web development experience, in addition to bridging the knowledge gap between college courses and industry practice! \n\nDesigners will learn hard skills such as research, wireframing, prototyping in Figma, and usability testing. Developers will get hands on experience with full-stack technologies and version control. \n\nCTC also provides a community of passionate, like-minded people that support each other through the learning process & life in general. As a member, you can expect team and club-wide socials, as well as a welcoming community!`,
+  },
+  {
+    question:
+      "How much time will I need to commit to Commit the Change per week?",
+    answer: `We aim to provide members of CTC with hands-on web development experience, in addition to bridging the knowledge gap between college courses and industry practice! \n\nDesigners will learn hard skills such as research, wireframing, prototyping in Figma, and usability testing. Developers will get hands on experience with full-stack technologies and version control. \n\nCTC also provides a community of passionate, like-minded people that support each other through the learning process & life in general. As a member, you can expect team and club-wide socials, as well as a welcoming community!`,
+  },
+  {
+    question: "What is a Coffee Chat?",
+    answer: `We aim to provide members of CTC with hands-on web development experience, in addition to bridging the knowledge gap between college courses and industry practice! \n\nDesigners will learn hard skills such as research, wireframing, prototyping in Figma, and usability testing. Developers will get hands on experience with full-stack technologies and version control. \n\nCTC also provides a community of passionate, like-minded people that support each other through the learning process & life in general. As a member, you can expect team and club-wide socials, as well as a welcoming community!`,
+  },
+  {
+    question: "What is a Skill Showcase?",
+    answer: `We aim to provide members of CTC with hands-on web development experience, in addition to bridging the knowledge gap between college courses and industry practice! \n\nDesigners will learn hard skills such as research, wireframing, prototyping in Figma, and usability testing. Developers will get hands on experience with full-stack technologies and version control. \n\nCTC also provides a community of passionate, like-minded people that support each other through the learning process & life in general. As a member, you can expect team and club-wide socials, as well as a welcoming community!`,
+  },
+  {
+    question: "Is there a skill or experience requirement to join?",
+    answer: `We aim to provide members of CTC with hands-on web development experience, in addition to bridging the knowledge gap between college courses and industry practice! \n\nDesigners will learn hard skills such as research, wireframing, prototyping in Figma, and usability testing. Developers will get hands on experience with full-stack technologies and version control. \n\nCTC also provides a community of passionate, like-minded people that support each other through the learning process & life in general. As a member, you can expect team and club-wide socials, as well as a welcoming community!`,
+  },
+];
