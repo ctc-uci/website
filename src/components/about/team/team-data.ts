016b6df2b1ca9251fcabac116e38a9061d4035b4
Lindsay Lim <limlindsayj@gmail.com>
2024-11-21 11:20:50 -0800

    Add Lindsay Lim member image

--- src/components/about/team/team-data.ts
+++ src/components/about/team/team-data.ts
@@ -105,6 +105,12 @@ export const CCH_DATA: Profile[] = [
     linkedin: "https://www.linkedin.com/in/sean-kelman-633057211/",
     image: "/people/sean-kelman.jpg",
   },
+  {
+    name: "Lindsay Lim",
+    role: "Developer",
+    linkedin: "https://www.linkedin.com/in/lindsay-lim-4b29332a2/",
+    image: "/people/lindsay-lim.jpg",
+  },
 ];
 
 export const CSE_DATA: Profile[] = [
