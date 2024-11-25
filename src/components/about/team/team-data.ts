f7870b974db38de5fbfd6390487f31ce9c36895c
SoulSen <talukdershowmen@gmail.com>
2024-11-24 22:05:05 -0800

    add showmen to cch projects tab

--- src/components/about/team/team-data.ts
+++ src/components/about/team/team-data.ts
@@ -105,6 +105,12 @@ export const CCH_DATA: Profile[] = [
     linkedin: "https://www.linkedin.com/in/sean-kelman-633057211/",
     image: "/people/sean-kelman.jpg",
   },
+  {
+    name: "Showmen Talukder",
+    role: "Developer",
+    linkedin: "https://www.linkedin.com/in/showmen-talukder-91b58820b/",
+    image: "/people/showmen-talukder.jpg",
+  },
 ];
 
 export const CSE_DATA: Profile[] = [
