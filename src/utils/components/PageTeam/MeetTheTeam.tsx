4966eed4022db76ea1ead776a1b65b73741e8455
Joshua Sullivan <joshee.sullivan@gmail.com>
2026-09-10 21:12:44 -0700

    chore: added missing photos, pd team, new temporary leads tab

--- src/utils/components/PageTeam/MeetTheTeam.tsx
+++ src/utils/components/PageTeam/MeetTheTeam.tsx
@@ -2,36 +2,35 @@ import { Text, Box, Container, VStack } from "@chakra-ui/react";
 import React from "react";
 
 import {
- BOARD_DATA,
- CLCHC_DATA,
- GCF_DATA,
- ELDR_DATA,
+  BOARD_DATA,
+  PD_TEAM_DATA,
+  LEAD_DATA
 } from "@/utils/constants/team-data";
 
 import { TeamDisplay } from "./TeamDisplay";
 
 export const MeetTheTeam: React.FC = () => {
- const labels = ["Board", "CLCHC", "GCF", "ELDR"];
+  const labels = ["Board", "PD Team", "Leads"];
 
- const teamData = [BOARD_DATA, CLCHC_DATA, GCF_DATA, ELDR_DATA];
+  const teamData = [BOARD_DATA, PD_TEAM_DATA, LEAD_DATA];
 
- return (
-  <Container maxW="7xl" py={12}>
-   <VStack spacing={8} align="stretch">
-    <Box textAlign="center">
-     <Text
-      fontSize={{ base: "4xl", md: "6xl", lg: "7xl" }}
-      fontWeight="bold"
-      mb={4}
-     >
-      Meet the Team
-     </Text>
-    </Box>
+  return (
+    <Container maxW="7xl" py={12}>
+      <VStack spacing={8} align="stretch">
+        <Box textAlign="center">
+          <Text
+            fontSize={{ base: "4xl", md: "6xl", lg: "7xl" }}
+            fontWeight="bold"
+            mb={4}
+          >
+            Meet the Team
+          </Text>
+        </Box>
 
-    <TeamDisplay teamData={teamData} labels={labels} />
-   </VStack>
-  </Container>
- );
+        <TeamDisplay teamData={teamData} labels={labels} />
+      </VStack>
+    </Container>
+  );
 };
 
 export default MeetTheTeam;
